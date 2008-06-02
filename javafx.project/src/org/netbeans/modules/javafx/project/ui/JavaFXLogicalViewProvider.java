bf9a06732b9b12fad716f956c0f40cabbd9dc115
kaktus <kaktus@netbeans.org>
2008-06-02 18:37:22 +0400

    Removed icon badging logic from project root node.

--- javafx.project/src/org/netbeans/modules/javafx/project/ui/JavaFXLogicalViewProvider.java
+++ javafx.project/src/org/netbeans/modules/javafx/project/ui/JavaFXLogicalViewProvider.java
@@ -260,25 +260,10 @@ public class JavaFXLogicalViewProvider implements LogicalViewProvider {
     
     /** Filter node containin additional features for the J2SE physical
      */
-    private final class JavaFXLogicalViewRootNode extends AbstractNode implements Runnable, FileStatusListener, ChangeListener, PropertyChangeListener {
-        
-        private Image icon;
-        private Lookup lookup;
+    private final class JavaFXLogicalViewRootNode extends AbstractNode {        
         private Action brokenLinksAction;
         private boolean broken;         //Represents a state where project has a broken reference repairable by broken reference support
         private boolean illegalState;   //Represents a state where project is not in legal state, eg invalid source/target level
-        
-        // icon badging >>>
-        private Set<FileObject> files;
-        private Map<FileSystem,FileStatusListener> fileSystemListeners;
-        private RequestProcessor.Task task;
-        private final Object privateLock = new Object();
-        private boolean iconChange;
-        private boolean nameChange;
-        private ChangeListener sourcesListener;
-        private Map<SourceGroup,PropertyChangeListener> groupsListeners;
-        //private Project project;
-        // icon badging <<<
         
         public JavaFXLogicalViewRootNode() {
             super(NodeFactorySupport.createCompositeChildren(project, "Projects/org-netbeans-modules-javafx-project/Nodes"), 
@@ -292,68 +277,6 @@ public class JavaFXLogicalViewProvider implements LogicalViewProvider {
                 illegalState = true;
             }
             brokenLinksAction = new BrokenLinksAction();
-            setProjectFiles(project);
-        }
-        
-        
-        protected final void setProjectFiles(Project project) {
-            Sources sources = ProjectUtils.getSources(project);  // returns singleton
-            if (sourcesListener == null) {
-                sourcesListener = WeakListeners.change(this, sources);
-                sources.addChangeListener(sourcesListener);
-            }
-            setGroups(Arrays.asList(sources.getSourceGroups(Sources.TYPE_GENERIC)));
-        }
-        
-        
-        private final void setGroups(Collection<SourceGroup> groups) {
-            if (groupsListeners != null) {
-                for (Map.Entry<SourceGroup,PropertyChangeListener> e : groupsListeners.entrySet()) {
-                    e.getKey().removePropertyChangeListener(e.getValue());
-                }
-            }
-            groupsListeners = new HashMap<SourceGroup,PropertyChangeListener>();
-            Set<FileObject> roots = new HashSet<FileObject>();
-            for (SourceGroup group : groups) {
-                PropertyChangeListener pcl = WeakListeners.propertyChange(this, group);
-                groupsListeners.put(group, pcl);
-                group.addPropertyChangeListener(pcl);
-                FileObject fo = group.getRootFolder();
-                roots.add(fo);
-            }
-            setFiles(roots);
-        }
-        
-        protected final void setFiles(Set<FileObject> files) {
-            if (fileSystemListeners != null) {
-                for (Map.Entry<FileSystem,FileStatusListener> e : fileSystemListeners.entrySet()) {
-                    e.getKey().removeFileStatusListener(e.getValue());
-                }
-            }
-            
-            fileSystemListeners = new HashMap<FileSystem,FileStatusListener>();
-            this.files = files;
-            if (files == null) {
-                return;
-            }
-            
-            Set<FileSystem> hookedFileSystems = new HashSet<FileSystem>();
-            for (FileObject fo : files) {
-                try {
-                    FileSystem fs = fo.getFileSystem();
-                    if (hookedFileSystems.contains(fs)) {
-                        continue;
-                    }
-                    hookedFileSystems.add(fs);
-                    FileStatusListener fsl = FileUtil.weakFileStatusListener(this, fs);
-                    fs.addFileStatusListener(fsl);
-                    fileSystemListeners.put(fs, fsl);
-                } catch (FileStateInvalidException e) {
-                    ErrorManager err = ErrorManager.getDefault();
-                    err.annotate(e, ErrorManager.UNKNOWN, "Cannot get " + fo + " filesystem, ignoring...", null, null, null); // NO18N
-                    err.notify(ErrorManager.INFORMATIONAL, e);
-                }
-            }
         }
         
         public String getHtmlDisplayName() {
@@ -368,91 +291,15 @@ public class JavaFXLogicalViewProvider implements LogicalViewProvider {
         }
         
         public Image getIcon(int type) {
-            Image img = getMyIcon(type);
-            
-            if (files != null && !files.isEmpty()) {
-                try {
-                    FileObject fo = files.iterator().next();
-                    img = fo.getFileSystem().getStatus().annotateIcon(img, type, files);
-                } catch (FileStateInvalidException e) {
-                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
-                }
-            }
-            
-            return img;
-        }
-        
-        private Image getMyIcon(int type) {
             Image original = super.getIcon(type);
             return broken || illegalState ? Utilities.mergeImages(original, brokenProjectBadge, 8, 0) : original;
         }
         
         public Image getOpenedIcon(int type) {
-            Image img = getMyOpenedIcon(type);
-            
-            if (files != null && !files.isEmpty()) {
-                try {
-                    FileObject fo = files.iterator().next();
-                    img = fo.getFileSystem().getStatus().annotateIcon(img, type, files);
-                } catch (FileStateInvalidException e) {
-                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
-                }
-            }
-            
-            return img;
-        }
-        
-        private Image getMyOpenedIcon(int type) {
             Image original = super.getOpenedIcon(type);
             return broken || illegalState ? Utilities.mergeImages(original, brokenProjectBadge, 8, 0) : original;
         }
         
-        public void run() {
-            boolean fireIcon;
-            boolean fireName;
-            synchronized (privateLock) {
-                fireIcon = iconChange;
-                fireName = nameChange;
-                iconChange = false;
-                nameChange = false;
-            }
-            if (fireIcon) {
-                fireIconChange();
-                fireOpenedIconChange();
-            }
-            if (fireName) {
-                fireDisplayNameChange(null, null);
-            }
-        }
-        
-        public void annotationChanged(FileStatusEvent event) {
-            if (task == null) {
-                task = RequestProcessor.getDefault().create(this);
-            }
-            
-            synchronized (privateLock) {
-                if ((iconChange == false && event.isIconChange()) || (nameChange == false && event.isNameChange())) {
-                    for (FileObject fo : files) {
-                        if (event.hasChanged(fo)) {
-                            iconChange |= event.isIconChange();
-                            nameChange |= event.isNameChange();
-                        }
-                    }
-                }
-            }
-            
-            task.schedule(50); // batch by 50 ms
-        }
-        
-        // sources change
-        public void stateChanged(ChangeEvent e) {
-            setProjectFiles(project);
-        }
-        
-        // group change
-        public void propertyChange(PropertyChangeEvent evt) {
-            setProjectFiles(project);
-        }
         
         public Action[] getActions( boolean context ) {
             return getAdditionalActions();
@@ -469,18 +316,6 @@ public class JavaFXLogicalViewProvider implements LogicalViewProvider {
         public HelpCtx getHelpCtx() {
             return new HelpCtx(JavaFXLogicalViewRootNode.class);
         }
-        
-        /*
-        public boolean canDestroy() {
-            return true;
-        }
-         
-        public void destroy() throws IOException {
-            System.out.println("Destroy " + project.getProjectDirectory());
-            LogicalViews.closeProjectAction().actionPerformed(new ActionEvent(this, 0, ""));
-            project.getProjectDirectory().delete();
-        }
-         */
         
         // Private methods -------------------------------------------------
         
@@ -551,10 +386,6 @@ public class JavaFXLogicalViewProvider implements LogicalViewProvider {
             }
         }        
 
-        private boolean isBroken() {
-            return this.broken;
-        }
-        
         private void setBroken(boolean broken) {
             this.broken = broken;
             brokenLinksAction.setEnabled(broken);
