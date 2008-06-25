55f54d1260c8c4e5f8296f743ebb7d4574a568ea
manowar <manowar@netbeans.org>
2008-06-25 15:55:03 +0200

    rollback to make it compilable for misk

--- javafx.navigation/src/org/netbeans/modules/javafx/navigation/base/TapPanel.java
+++ javafx.navigation/src/org/netbeans/modules/javafx/navigation/base/TapPanel.java
@@ -87,7 +87,6 @@ public final class TapPanel extends javax.swing.JPanel {
             ((TapPanel) e.getSource()).setArmed(false);
         }
 
-        @Override
         public void mouseMoved(MouseEvent e) {
             ((TapPanel) e.getSource()).setArmed(((TapPanel) e.getSource()).isArmPoint(e.getPoint()));
         }
@@ -102,7 +101,6 @@ public final class TapPanel extends javax.swing.JPanel {
             }
         }
 
-        @Override
         public void mouseDragged(MouseEvent e) {
             //do nothing
         }
