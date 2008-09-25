c6e591340a1a2e7a58de06ec43939c47386fd5ca
cms <cms@netbeans.org>
2008-09-25 17:54:22 +0400

    Fix for 140257

--- javafx.profiler/src/org/netbeans/modules/javafx/profiler/utilities/GoToJavaFXSourceProvider.java
+++ javafx.profiler/src/org/netbeans/modules/javafx/profiler/utilities/GoToJavaFXSourceProvider.java
@@ -71,19 +71,12 @@ public class GoToJavaFXSourceProvider implements GoToSourceProvider {
 
         Iterator<FileObject> files = JavaFXProjectUtilities.getSourceFiles((JavaFXProject)project).iterator();
         JavaFXSource source = null;
-        String cName = className.substring(className.lastIndexOf('.') + 1, className.length());
-        if (cName.indexOf('$') != -1)
-            cName = cName.substring(0, cName.indexOf('$'));
 
         while(files.hasNext()) {
             FileObject fo = files.next();
-            if(fo.getName().equals(cName)) {
-                source = JavaFXSource.forFileObject(fo);
-            }
-        }
-
-        if (source == null) {
-            return false;
+            source = JavaFXSource.forFileObject(fo);
+            if (source != null)
+                break;
         }
 
         final JavaFXSource js = source;
@@ -109,7 +102,7 @@ public class GoToJavaFXSourceProvider implements GoToSourceProvider {
                         if (!controller.getJavafxTypes().isJFXClass((Symbol) classElement)) {
                             return;
                         }
-                            
+
                         if ((methodName != null) && (methodName.length() > 0)) {
                             // if a method name has been specified try to resolve the method
                             if (classElement != null) {
