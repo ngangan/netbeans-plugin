6d8f7d2064e8631777ae84ac12fa15df091dce2f
misk <misk@netbeans.org>
2008-09-29 14:33:52 +0200

    Update to latest version of javafx runtime

--- javafx.palette/src/org/netbeans/modules/javafx/palette/items/applications/Stage.java
+++ javafx.palette/src/org/netbeans/modules/javafx/palette/items/applications/Stage.java
@@ -50,16 +50,17 @@ import org.openide.util.NbBundle;
  *
  * @author Michal Skvor
  */
-public class Application implements ActiveEditorDrop {
+public class Stage implements ActiveEditorDrop {
 
     public boolean handleTransfer(JTextComponent targetComponent) {
-        String code = NbBundle.getMessage( Application.class, "TEMPLATE_Application"  ); // NOI18N
+        String code = NbBundle.getMessage( Stage.class, "TEMPLATE_Stage"    ); // NOI18N
         CodeTemplateManager ctm = CodeTemplateManager.get( targetComponent.getDocument());
         CodeTemplate template = ctm.createTemporary( code );
         template.insert( targetComponent );
         
         // Imports
-        Util.addImport( targetComponent, "javafx.stage.Application" );
+        Util.addImport( targetComponent, "javafx.stage.Stage" );
+        Util.addImport( targetComponent, "javafx.stage.Decoration" );
         Util.addImport( targetComponent, "javafx.scene.Scene" );
         return true;
     }
