193b18afabe74e216e59686476edfbd6b944ff83
manowar <manowar@netbeans.org>
2008-09-29 16:14:32 +0200

    API cleanup

--- javafx.editor/src/org/netbeans/api/javafx/editor/FXSourceUtils.java
+++ javafx.editor/src/org/netbeans/api/javafx/editor/FXSourceUtils.java
@@ -24,15 +24,16 @@ import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.TypeParameterElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.ArrayType;
 import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import org.netbeans.api.java.classpath.ClassPath;
 import org.netbeans.api.java.queries.JavadocForBinaryQuery;
 import org.netbeans.api.java.queries.SourceForBinaryQuery;
 import org.netbeans.api.javafx.source.ClasspathInfo;
-import org.netbeans.api.javafx.source.CompilationInfo;
 import org.netbeans.modules.javafx.source.classpath.FileObjects;
 import org.netbeans.spi.java.classpath.support.ClassPathSupport;
 import org.openide.filesystems.FileObject;
@@ -49,86 +50,315 @@ public final class FXSourceUtils {
     private static final char[] CODE_COMPL_SUBST_BREAKERS = {';', '.', ',', '+', '-', '/', '%', '^', '|', '&', '(', ')', '{', '}', ' ', '\t', '\n', '\r'}; // NOI18N
     private static final String PACKAGE_SUMMARY = "package-summary"; // NOI18N
 
+    // colors for navigator
+    private static final String TYPE_COLOR = "#707070"; // NOI18N
+    private static final String INHERITED_COLOR = "#7D694A"; // NOI18N
+
     private FXSourceUtils() {
     }
 
-    // TODO move it from GoToSupport
-    public static String getElementTooltip(CompilationInfo info, Element elem) {
-        JavafxTypes types = info.getJavafxTypes();
-        if (elem instanceof VariableElement) {
-            String prefix = "<html>";
-
-            VariableElement var = (VariableElement) elem;
+    /// Editor tooltips
+    /// =====================
+    public static String getElementTooltip(JavafxTypes types, Element element) {
+        if (element instanceof VariableElement) {
+            String prefix = "<html>"; // NOI18N
+            VariableElement var = (VariableElement) element;
             ElementKind kind = var.getKind();
             if (kind == ElementKind.FIELD) {
-                prefix = prefix + var.getEnclosingElement() + ".";
+                prefix = prefix + var.getEnclosingElement() + '.'; // NOI18N
             }
 
-            Symbol sym = (Symbol) elem;
+            Symbol sym = (Symbol) element;
             Type type = sym.asType();
-            return prefix + "<b>" + var + "</b> : " + FXSourceUtils.typeToString(types, type);
+
+            StringBuilder s = new StringBuilder();
+            s.append(prefix);
+            s.append("<b>"); // NOI18N
+            s.append(var);
+            s.append("</b> : "); // NOI18N
+            s.append(typeToString(types, type));
+
+            return s.toString();
         }
 
-        if (elem instanceof TypeElement) {
-            return elem.toString();
+        if (element instanceof TypeElement) {
+            return element.toString(); // TODO use typeElementToString() ?
         }
 
-        if (elem instanceof ExecutableElement) {
-            ExecutableElement var = (ExecutableElement) elem;
-            Symbol sym = (Symbol) elem;
+        if (element instanceof ExecutableElement) {
+            ExecutableElement var = (ExecutableElement) element;
+            Symbol sym = (Symbol) element;
             Type type = sym.asType();
-            return "<html>" + var.getEnclosingElement() + ".<b>" + sym.name.toString() + "</b>" + FXSourceUtils.methodToString(types, (MethodType) type);
+            
+            StringBuilder s = new StringBuilder();
+            s.append("<html>"); // NOI18N
+            s.append(var.getEnclosingElement());
+            s.append(".<b>"); // NOI18N
+            s.append(sym.name);
+            s.append("</b>"); // NOI18N
+            s.append(methodToString(types, (MethodType) type));
+
+            return s.toString();
         }
-        
+
         return null;
     }
 
-    public static String typeToString(JavafxTypes types, Type type) {
-        String suffix = "";
+    private static String typeToString(JavafxTypes types, Type type) {
+        String suffix = ""; // NOI18N
         if (type instanceof FunctionType) {
             MethodType mtype = ((FunctionType) type).asMethodType();
-            return "function" + methodToString(types, mtype);
+            return "function" + methodToString(types, mtype); // NOI18N
         }
 
         if (types.isSequence(type)) {
-            suffix = "[ ]";
+            suffix = "[ ]"; // NOI18N
             type = types.elementType(type);
         }
         switch (type.tag) {
             case TypeTags.DOUBLE:
-                return "Number" + suffix;
+                return "Number" + suffix; // NOI18N
 
             case TypeTags.INT:
-                return "Integer" + suffix;
+                return "Integer" + suffix; // NOI18N
 
             case TypeTags.VOID:
-                return "Void" + suffix;
+                return "Void" + suffix; // NOI18N
 
             default:
                 return type.toString() + suffix;
         }
     }
 
-    public static String methodToString(JavafxTypes types, MethodType mtype) {
+    private static String methodToString(JavafxTypes types, MethodType mtype) {
         StringBuilder s = new StringBuilder();
-        s.append("(");
+        s.append('('); // NOI18N
         if (mtype == null) {
-            s.append("???");
+            s.append("???"); // NOI18N
         } else {
             List<Type> args = mtype.argtypes;
             for (List<Type> l = args; l.nonEmpty(); l = l.tail) {
                 if (l != args) {
-                    s.append(", ");
+                    s.append(", "); // NOI18N
                 }
-                s.append(':');
+                s.append(':'); // NOI18N
                 s.append(typeToString(types, l.head));
             }
         }
-        s.append("):");
-        s.append(mtype == null ? "???" : typeToString(types, mtype.restype));
+        s.append("):"); // NOI18N
+        s.append(mtype == null ? "???" : typeToString(types, mtype.restype)); // NOI18N
         return s.toString();
     }
 
+    /// Navigator
+    /// =====================
+    public static String executableElementToString(JavafxTypes types, ExecutableElement element, boolean isDeprecated, boolean isInherited) {
+        StringBuilder sb = new StringBuilder();
+        if (isDeprecated) {
+            sb.append("<s>"); // NOI18N
+        }
+        if (isInherited) {
+            sb.append("<font color="); // NOI18N
+            sb.append(INHERITED_COLOR);
+            sb.append('>'); // NOI18N
+        }
+        if (element.getKind() == ElementKind.CONSTRUCTOR) {
+            sb.append(element.getEnclosingElement().getSimpleName());
+        } else {
+            sb.append(element.getSimpleName());
+        }
+        if (isDeprecated) {
+            sb.append("</s>"); // NOI18N
+        }
+
+        sb.append('('); // NOI18N
+        java.util.List<? extends VariableElement> params = element.getParameters();
+        for (Iterator<? extends VariableElement> it = params.iterator(); it.hasNext();) {
+            VariableElement param = it.next();
+            sb.append(param.getSimpleName());
+            sb.append(": "); // NOI18N     
+            sb.append("<font color="); // NOI18N
+            sb.append(TYPE_COLOR);
+            sb.append('>'); // NOI18N
+            sb.append(typeToString(types, (Type) param.asType()));
+            sb.append("</font>"); // NOI18N
+            if (it.hasNext()) {
+                sb.append(", "); // NOI18N
+            }
+        }
+        sb.append(')'); // NOI18N
+
+        if (element.getKind() != ElementKind.CONSTRUCTOR) {
+            TypeMirror rt = element.getReturnType();
+            if (rt.getKind() != TypeKind.VOID) {
+                sb.append(": "); // NOI18N     
+                sb.append("<font color="); // NOI18N
+                sb.append(TYPE_COLOR);
+                sb.append('>'); // NOI18N
+                sb.append(typeToString(types, (Type) element.getReturnType()));
+                sb.append("</font>"); // NOI18N                    
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String variableElementToString(JavafxTypes types, VariableElement element, boolean isDeprecated, boolean isInherited) {
+        StringBuilder sb = new StringBuilder();
+
+        if (isDeprecated) {
+            sb.append("<s>"); // NOI18N
+        }
+        if (isInherited) {
+            sb.append("<font color="); // NOI18N
+            sb.append(INHERITED_COLOR);
+            sb.append('>'); // NOI18N
+        }
+        sb.append(element.getSimpleName());
+        if (isDeprecated) {
+            sb.append("</s>"); // NOI18N
+        }
+
+        if (element.getKind() != ElementKind.ENUM_CONSTANT) {
+            sb.append(": "); // NOI18N
+            sb.append("<font color="); // NOI18N
+            sb.append(TYPE_COLOR);
+            sb.append('>'); // NOI18N
+            final Type type = (Type) element.asType();
+            if (type instanceof FunctionType) {
+                MethodType mtype = ((FunctionType) type).asMethodType();
+                sb.append("function "); // NOI18N
+
+                sb.append('('); // NOI18N
+                com.sun.tools.javac.util.List<Type> args = mtype.argtypes;
+                for (com.sun.tools.javac.util.List<Type> l = args; l.nonEmpty(); l = l.tail) {
+                    if (l != args) {
+                        sb.append(", "); // NOI18N
+                    }
+                    sb.append(':'); // NOI18N
+                    sb.append("<font color="); // NOI18N
+                    sb.append(TYPE_COLOR);
+                    sb.append('>'); // NOI18N
+                    sb.append(typeToString(types, l.head));
+                    sb.append("</font>"); // NOI18N
+                }
+                sb.append(')'); // NOI18N
+
+                sb.append(": "); // NOI18N     
+                sb.append("<font color="); // NOI18N
+                sb.append(TYPE_COLOR);
+                sb.append('>'); // NOI18N
+                sb.append(typeToString(types, mtype.restype));
+                sb.append("</font>"); // NOI18N                    
+            } else {
+                sb.append(typeToString(types, type));
+            }
+            sb.append("</font>"); // NOI18N
+        }
+
+        return sb.toString();
+    }
+
+    public static String typeElementToString(JavafxTypes types, TypeElement element, boolean isDeprecated, boolean isInherited) {
+        StringBuilder sb = new StringBuilder();
+        if (isDeprecated) {
+            sb.append("<s>"); // NOI18N
+        }
+        if (isInherited) {
+            sb.append("<font color="); // NOI18N
+            sb.append(INHERITED_COLOR);
+            sb.append('>'); // NOI18N
+        }
+        sb.append(element.getSimpleName());
+        if (isDeprecated) {
+            sb.append("</s>"); // NOI18N
+        }
+
+        java.util.List<? extends TypeParameterElement> typeParams = element.getTypeParameters();
+        if (typeParams != null && !typeParams.isEmpty()) {
+            sb.append("&lt;"); // NOI18N
+
+            for (Iterator<? extends TypeParameterElement> it = typeParams.iterator(); it.hasNext();) {
+                TypeParameterElement tp = it.next();
+                sb.append(tp.getSimpleName());
+                try {
+                    java.util.List<? extends TypeMirror> bounds = tp.getBounds();
+                    if (!bounds.isEmpty()) {
+                        sb.append(boundsToString(bounds, types));
+                    }
+                } catch (NullPointerException npe) {
+                    npe.printStackTrace(); // TODO
+                }
+                if (it.hasNext()) {
+                    sb.append(", "); // NOI18N
+                }
+            }
+
+            sb.append("&gt;"); // NOI18N
+        }
+
+        // Add superclass and implemented interfaces
+        TypeMirror sc = element.getSuperclass();
+        String scName = typeToString(types, (Type) sc);
+
+        if (sc == null || element.getKind() == ElementKind.ENUM || element.getKind() == ElementKind.ANNOTATION_TYPE ||
+                "Object".equals(scName) || // NOI18N
+                "<none>".equals(scName)) { // NOI18N
+            scName = null;
+        }
+
+        java.util.List<? extends TypeMirror> ifaces = element.getInterfaces();
+        if ((scName != null || !ifaces.isEmpty()) && element.getKind() != ElementKind.ANNOTATION_TYPE) {
+            sb.append(" :: "); // NOI18N
+            if (scName != null) {
+                sb.append("<font color="); // NOI18N
+                sb.append(TYPE_COLOR);
+                sb.append('>'); // NOI18N
+                sb.append(scName);
+                sb.append("</font>"); // NOI18N
+            }
+            
+            if (!ifaces.isEmpty()) {
+                if (scName != null) {
+                    sb.append(" : "); // NOI18N
+                }
+                
+                for (Iterator<? extends TypeMirror> it = ifaces.iterator(); it.hasNext();) {
+                    TypeMirror typeMirror = it.next();
+                    sb.append("<font color="); // NOI18N
+                    sb.append(TYPE_COLOR);
+                    sb.append('>'); // NOI18N
+                    sb.append(typeToString(types, (Type) typeMirror));
+                    sb.append("</font>"); // NOI18N
+                    if (it.hasNext()) {
+                        sb.append(", "); // NOI18N
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static String boundsToString(java.util.List<? extends TypeMirror> bounds, JavafxTypes types) {
+        if (bounds.size() == 1 && "java.lang.Object".equals(bounds.get(0).toString())) { // NOI18N
+            return ""; // NOI18N
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(" extends "); // NOI18N
+        for (Iterator<? extends TypeMirror> it = bounds.iterator(); it.hasNext();) {
+            TypeMirror bound = it.next();
+            sb.append(typeToString(types, (Type) bound));
+            if (it.hasNext()) {
+                sb.append(" & "); // NOI18N
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /// =====================
     public static int getSubstitutionLenght(final String text, final int offset, int length) {
         if (text == null) {
             return length;
@@ -327,5 +557,4 @@ public final class FXSourceUtils {
                 sb.append(type);
         }
     }
-    
 }
