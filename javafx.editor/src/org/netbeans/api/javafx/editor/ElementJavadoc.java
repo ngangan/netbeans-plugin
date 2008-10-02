8e6a5b4111078c503ea0fb9bb5e980c3a60eae51
manowar <manowar@netbeans.org>
2008-10-02 12:00:46 +0200

    fixing compilation error in Utilities and assertation error in ElementJavaDoc

--- javafx.editor/src/org/netbeans/api/javafx/editor/ElementJavadoc.java
+++ javafx.editor/src/org/netbeans/api/javafx/editor/ElementJavadoc.java
@@ -663,11 +663,12 @@ public class ElementJavadoc {
         StringBuilder sb = new StringBuilder();
         sb.append("<p><tt>"); //NOI18N
         sb.append(getAnnotations(eu, cdoc.annotations()));
-        int mods = cdoc.modifierSpecifier() & ~Modifier.INTERFACE;
-        if (cdoc.isEnum()) {
-            mods &= ~Modifier.FINAL;
-        }
-        sb.append(Modifier.toString(mods));
+        // TODO modifierSpecifier() does not exist for javafx
+//        int mods = cdoc.modifierSpecifier() & ~Modifier.INTERFACE;
+//        if (cdoc.isEnum()) {
+//            mods &= ~Modifier.FINAL;
+//        }
+//        sb.append(Modifier.toString(mods));
         if (sb.length() > 0) {
             sb.append(' '); //NOI18N
         }
