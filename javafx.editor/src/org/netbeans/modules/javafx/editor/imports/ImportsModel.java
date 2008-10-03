9d349627533641254c923ded2fad2a48e26e0790
Rastislav Komara" <moonko@netbeans.org>
2008-10-03 10:32:02 +0200

    Explicit locking added around reformat call. Fixing issue #148966

--- javafx.editor/src/org/netbeans/modules/javafx/editor/imports/ImportsModel.java
+++ javafx.editor/src/org/netbeans/modules/javafx/editor/imports/ImportsModel.java
@@ -14,6 +14,7 @@ import org.netbeans.api.javafx.source.ElementHandle;
 import org.netbeans.api.lexer.TokenHierarchy;
 import org.netbeans.api.lexer.TokenId;
 import org.netbeans.api.lexer.TokenSequence;
+import org.netbeans.editor.GuardedDocument;
 import org.netbeans.modules.editor.indent.api.Reformat;
 import org.netbeans.modules.javafx.editor.JFXImportManager;
 import org.netbeans.modules.javafx.editor.imports.ui.FixImportsLayout;
@@ -163,26 +164,45 @@ public final class ImportsModel {
 
 
     void publish(final Document doc) {
-        Collections.sort(entries);
-        TokenSequence<JFXTokenId> ts = getTokenSequence(doc, 0);
-        final int startPos = quessImportsStart(ts);
-        final int endPos = quessImportsEnd(ts, startPos);
-        try {
-            Position end = doc.createPosition(endPos);
-            logger.info("Publishing following entries:");
-            doc.remove(startPos, endPos - startPos);
-            int offset = startPos;
-            boolean first = true;
-            for (ModelEntry entry : entries) {
-                logger.info("\t" + entry.toImportStatement());
-                String text = (first ? "" : "\n") + entry.toImportStatement();
-                first = false;
-                doc.insertString(offset, text, null);
-                offset += text.length();
+        Runnable runnable = new Runnable() {
+            public void run() {
+                Collections.sort(entries);
+                TokenSequence<JFXTokenId> ts = getTokenSequence(doc, 0);
+                final int startPos = quessImportsStart(ts);
+                final int endPos = quessImportsEnd(ts, startPos);
+                Reformat reformat = null;
+                boolean relock = !(doc instanceof GuardedDocument);
+                try {
+                    Position end = doc.createPosition(endPos);
+                    logger.info("Publishing following entries:");
+                    doc.remove(startPos, endPos - startPos);
+                    int offset = startPos;
+                    boolean first = true;
+                    for (ModelEntry entry : entries) {
+                        logger.info("\t" + entry.toImportStatement());
+                        String text = (first ? "" : "\n") + entry.toImportStatement();
+                        first = false;
+                        doc.insertString(offset, text, null);
+                        offset += text.length();
+                    }
+                    reformat = Reformat.get(doc);
+                    reformat.lock();
+                    reformat.reformat(0, end.getOffset());
+                } catch (BadLocationException e) {
+                    logger.severe(e.getLocalizedMessage());
+                } finally {
+                    if (reformat != null) {
+                        reformat.unlock();
+                    }
+                }
             }
-            Reformat.get(doc).reformat(0, end.getOffset());
-        } catch (BadLocationException e) {
-            logger.severe(e.getLocalizedMessage());
+        };
+        if (doc instanceof GuardedDocument) {
+            GuardedDocument gd = (GuardedDocument) doc;
+            gd.runAtomic(runnable);
+        } else {
+            logger.warning("Running in non atomic fashion.");
+            runnable.run();
         }
 
     }
