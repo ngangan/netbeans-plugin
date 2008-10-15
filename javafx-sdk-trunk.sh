3790fced448a1f4eb7242dc37393b2fc94c1e51f
Martin Ryzl <mryzl@netbeans.org>
2008-10-15 14:14:42 +0200

    sdk url scripts added

---
+++ javafx-sdk-trunk.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# This script is purposed to set correct URL of JavaFX SDK
+#
+#  (c) Martin Ryzl, SUN Microsystems, Oct 2008
+#########################################################################
+
+#!/bin/bash
+
+export SDK_URL=http://getjfx.sfbay.sun.com/hudson/job/javafx1.0/lastSuccessfulBuild
+
