0f67cee87ecc81231bb26ed2f1cd2bdfaf55e19d
redtigra <redtigra@netbeans.org>
2008-10-15 16:07:08 +0400

    Minor changes in javafx-nb-plugin-build.sh

--- javafx-nb-plugin-build.sh
+++ javafx-nb-plugin-build.sh
@@ -104,6 +104,4 @@ cd $WORKSPACE/main/nbbuild
 mkdir -p nbms/compress
 zip nbms/compress/${BUILD_ID}.zip nbms/javafx2/*.nbm
 tar cvf - nbms/javafx2/*.nbm |  gzip  > nbms/compress/${BUILD_ID}.tar.gz
-ln -s $WORKSPACE/main/nbbuild/nbms/compress/${BUILD_ID}.zip nbms/compress/netbeans-6.5-javafx-nbms.zip
-ln -s $WORKSPACE/main/nbbuild/nbms/compress/${BUILD_ID}.tar.gz nbms/compress/netbeans-6.5-javafx-nbms.tar.gz
 
