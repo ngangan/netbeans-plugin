8733da50726de53c131f851c610f72de06d83a15
Martin Ryzl <mryzl@netbeans.org>
2008-10-20 15:38:34 +0200

    promoted build updated to b42

--- javafx-sdk-promoted.sh
+++ javafx-sdk-promoted.sh
@@ -5,5 +5,6 @@
 #########################################################################
 
 #!/bin/bash
-export SDK_URL=http://getjfx.sfbay.sun.com/hudson/job/javafx1.0/741/
+echo Promoted JavaFX SDK build b42 (749)
+export SDK_URL=http://getjfx.sfbay.sun.com/hudson/job/javafx1.0/749/
 sh -x $WORKSPACE/main/contrib/javafx-nb-plugin-build.sh
