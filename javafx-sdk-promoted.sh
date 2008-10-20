d7998390f893c45787252032bd32698a647c5096
Martin Ryzl <mryzl@netbeans.org>
2008-10-20 17:00:06 +0200

    fixed typo

--- javafx-sdk-promoted.sh
+++ javafx-sdk-promoted.sh
@@ -5,6 +5,6 @@
 #########################################################################
 
 #!/bin/bash
-echo Promoted JavaFX SDK build b42 (749)
+echo "Promoted JavaFX SDK build b42 (749)"
 export SDK_URL=http://getjfx.sfbay.sun.com/hudson/job/javafx1.0/749/
 sh -x $WORKSPACE/main/contrib/javafx-nb-plugin-build.sh
