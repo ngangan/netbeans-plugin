1c585070a4a6ecc1d5eba7f0b1b31efaa81d90c2
asotona <asotona@netbeans.org>
2009-03-11 18:12:56 +0100

    fixed preview layout

--- javafx.editor/src/org/netbeans/modules/javafx/editor/preview/JavaFXPreviewTopComponent.java
+++ javafx.editor/src/org/netbeans/modules/javafx/editor/preview/JavaFXPreviewTopComponent.java
@@ -141,16 +141,7 @@ public final class JavaFXPreviewTopComponent extends TopComponent implements Pro
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setLayout(new java.awt.GridLayout());
     }// </editor-fold>//GEN-END:initComponents
 
 
