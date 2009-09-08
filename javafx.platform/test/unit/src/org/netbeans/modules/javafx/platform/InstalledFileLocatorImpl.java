c9a02fc37a3e9f78faa08da4f30fdbf70cdca825
Martin Krauskopf <mkrauskopf@netbeans.org>
2009-09-08 09:33:13 +0200

    Forgotten debug output.

--- javafx.platform/test/unit/src/org/netbeans/modules/javafx/platform/InstalledFileLocatorImpl.java
+++ javafx.platform/test/unit/src/org/netbeans/modules/javafx/platform/InstalledFileLocatorImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -22,7 +22,7 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -43,10 +43,6 @@ import org.openide.modules.InstalledFileLocator;
 
 @org.openide.util.lookup.ServiceProvider(service=org.openide.modules.InstalledFileLocator.class)
 public final class InstalledFileLocatorImpl extends InstalledFileLocator {
-
-    public InstalledFileLocatorImpl() {
-        System.out.println("created");
-    }
 
     public @Override File locate(String relativePath, String codeNameBase, boolean localized) {
         if ("javafx-sdk/lib/shared/javafxc.jar".equals(relativePath)) {
