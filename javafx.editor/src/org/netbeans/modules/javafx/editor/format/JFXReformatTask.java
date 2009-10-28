b11d7b3528497ffc8e224ae8c64b00f950069daf
manowar <manowar@netbeans.org>
2009-10-28 13:54:27 +0100

    issue #175584 has been fixed

--- javafx.editor/src/org/netbeans/modules/javafx/editor/format/JFXReformatTask.java
+++ javafx.editor/src/org/netbeans/modules/javafx/editor/format/JFXReformatTask.java
@@ -1672,6 +1672,9 @@ public class JFXReformatTask implements ReformatTask {
         public Boolean visitTypeClass(TypeClassTree node, Void p) {
             accept(JFXTokenId.IDENTIFIER);
             // sequence type
+//            if (accept(JFXTokenId.LBRACKET) == JFXTokenId.LBRACKET && cs.spaceWithinArrayInitBrackets()) {
+//                space();
+//            }
             accept(JFXTokenId.LBRACKET);
             spaces(cs.spaceWithinArrayInitBrackets() ? 1 : 0);
             accept(JFXTokenId.RBRACKET);
