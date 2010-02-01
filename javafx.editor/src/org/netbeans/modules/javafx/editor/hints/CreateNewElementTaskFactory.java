18013c4d31f2589f0de6918df24f48f715f8e276
Karol Harezlak <kharezlak@netbeans.org>
2010-02-01 17:25:16 +0100

    JavaFX editor hints update.

--- javafx.editor/src/org/netbeans/modules/javafx/editor/hints/CreateNewElementTaskFactory.java
+++ javafx.editor/src/org/netbeans/modules/javafx/editor/hints/CreateNewElementTaskFactory.java
@@ -5,26 +5,18 @@
 package org.netbeans.modules.javafx.editor.hints;
 
 import com.sun.javafx.api.tree.ClassDeclarationTree;
-import com.sun.javafx.api.tree.ErroneousTree;
 import com.sun.javafx.api.tree.FunctionInvocationTree;
 import com.sun.javafx.api.tree.IdentifierTree;
-import com.sun.javafx.api.tree.JavaFXTreePath;
 import com.sun.javafx.api.tree.JavaFXTreePathScanner;
-import com.sun.javafx.api.tree.LiteralTree;
 import com.sun.javafx.api.tree.SourcePositions;
 import com.sun.javafx.api.tree.Tree;
-import com.sun.javafx.api.tree.VariableInvalidateTree;
-import com.sun.javafx.api.tree.VariableTree;
-import com.sun.tools.javafx.code.JavafxClassSymbol;
-import com.sun.tools.javafx.tree.JFXClassDeclaration;
-import com.sun.tools.javafx.tree.JFXFunctionDefinition;
-import com.sun.tools.javafx.tree.JFXFunctionInvocation;
+import com.sun.tools.javafx.tree.JFXVar;
 import com.sun.tools.mjavac.util.JCDiagnostic;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
-import javax.lang.model.element.Element;
 import javax.swing.SwingUtilities;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 import javax.tools.Diagnostic;
@@ -34,7 +26,6 @@ import org.netbeans.api.javafx.source.Imports;
 import org.netbeans.api.javafx.source.JavaFXSource;
 import org.netbeans.api.javafx.source.support.EditorAwareJavaFXSourceTaskFactory;
 import org.netbeans.modules.javafx.editor.JavaFXDocument;
-import org.netbeans.modules.javafx.editor.imports.ui.FixItem;
 import org.netbeans.spi.editor.hints.ChangeInfo;
 import org.netbeans.spi.editor.hints.ErrorDescription;
 import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
@@ -42,6 +33,7 @@ import org.netbeans.spi.editor.hints.Fix;
 import org.netbeans.spi.editor.hints.HintsController;
 import org.netbeans.spi.editor.hints.Severity;
 import org.openide.filesystems.FileObject;
+import org.openide.util.Exceptions;
 
 /**
  *
@@ -52,9 +44,6 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
     private final AtomicBoolean cancel = new AtomicBoolean();
     private static final String ERROR_CODE = "compiler.err.cant.resolve.location"; //NOI18N
     private static final String HINT_IDENT = "createmethod"; //NOI18N
-    private static final String FUNCTION_VALUE = "function"; //NOI18N
-    private static final String CLASS_VALUE = "class"; //NOI18N
-    private static final String VAR_VALUE = "variable"; //NOI18N
     private static Logger log = Logger.getLogger(CreateNewElementTaskFactory.class.getName());
 
     public CreateNewElementTaskFactory() {
@@ -62,8 +51,11 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
     }
 
     private enum Kind {
-
-        CLASS, FUNCTION, VARIABLE
+        CLASS,
+        FUNCTION,
+        LOCAL_VARIABLE,
+        VARIABLE,
+        LOCAL_CLASS
     };
 
     @Override
@@ -89,7 +81,11 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
                             && !cancel.get()
                             && (diagnostic instanceof JCDiagnostic)) {
 
-                        errorDescriptions.add(getErrorDescription(document, compilationInfo, (JCDiagnostic) diagnostic));
+                        String message = diagnostic.getMessage(Locale.ENGLISH);
+                        Kind kinds[] = getKinds(message);
+                        for (Kind kind : kinds) {
+                            errorDescriptions.add(getErrorDescription(document, compilationInfo, (JCDiagnostic) diagnostic, kind));
+                        }
                     }
                 }
                 HintsController.setErrors(compilationInfo.getDocument(), HINT_IDENT, errorDescriptions);
@@ -112,41 +108,38 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
 
     private ErrorDescription getErrorDescription(JavaFXDocument document,
             final CompilationInfo compilationInfo,
-            final JCDiagnostic diagnostic) {
-
-
-        String message = diagnostic.getMessage(Locale.ENGLISH);
-        Kind kind = getKind(message);
+            final JCDiagnostic diagnostic,
+            final Kind kind) {
 
         if (kind == null) {
             return null;
         }
-        Fix fix = new VariableFix(kind, document, diagnostic, compilationInfo);
-        ErrorDescription errorDescription = ErrorDescriptionFactory.createErrorDescription(Severity.HINT, "Create missing elements", Collections.singletonList(fix), compilationInfo.getFileObject(), (int) diagnostic.getStartPosition(), (int) diagnostic.getStartPosition());
+        Fix fix = new ElementFix(kind, document, diagnostic, compilationInfo);
+        ErrorDescription errorDescription = ErrorDescriptionFactory.createErrorDescription(Severity.HINT, kind.toString(), Collections.singletonList(fix), compilationInfo.getFileObject(), (int) diagnostic.getStartPosition(), (int) diagnostic.getStartPosition());
 
         return errorDescription;
     }
 
-    private Kind getKind(String message) {
-        Kind kind = null;
-        if (message.contains("symbol  : class")) {
-            kind = Kind.CLASS;
-        } else if (message.contains("symbol  : variable")) {
-            kind = Kind.VARIABLE;
-        } else if (message.contains("symbol  : function")) {
-            kind = Kind.FUNCTION;
+    private Kind[] getKinds(String message) {
+        if (message.contains("symbol  : class")) { //NOI18N
+            return new Kind[]{Kind.LOCAL_CLASS};
+        } else if (message.contains("symbol  : variable")) { //NOI18N
+            return new Kind[]{Kind.VARIABLE, Kind.LOCAL_VARIABLE};
+        } else if (message.contains("symbol  : function")) { //NOI18N
+            return new Kind[]{Kind.FUNCTION}; //NOI18N
         }
-        return kind;
+
+        throw new IllegalStateException();
     }
 
-    private class VariableFix implements Fix {
+    private class ElementFix implements Fix {
 
         private final Kind kind;
         private final Document document;
         private final JCDiagnostic diagnostic;
         private final CompilationInfo compilationInfo;
 
-        public VariableFix(Kind kind, Document document, JCDiagnostic diagnostic, CompilationInfo compilationInfo) {
+        public ElementFix(Kind kind, Document document, JCDiagnostic diagnostic, CompilationInfo compilationInfo) {
             this.kind = kind;
             this.document = document;
             this.diagnostic = diagnostic;
@@ -154,20 +147,20 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
         }
 
         public String getText() {
-            return "Create " + kind;
+            return "Create " + kind; //TODO
         }
 
         public ChangeInfo implement() throws Exception {
             final GeneratedCode[] generatedCode = new GeneratedCode[1];
             if (kind == Kind.FUNCTION) {
                 generatedCode[0] = createFunction();
-            } else if (kind == Kind.VARIABLE) {
-                // generatedCode[0] = createVariable(diagnostic);
-            } else {
-                // generatedCode = createInnerClass();
+            } else if (kind == Kind.LOCAL_VARIABLE || kind == Kind.VARIABLE) {
+                generatedCode[0] = createVariable(diagnostic, kind);
+            } else if (kind == Kind.LOCAL_CLASS) {
+                generatedCode[0] = createLocalClass();
             }
             if (generatedCode[0] == null) {
-               return null;
+                return null;
             }
             SwingUtilities.invokeLater(new Runnable() {
 
@@ -193,7 +186,7 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
         }
 
         private GeneratedCode createFunction() {
-            StringBuffer function = new StringBuffer();
+            StringBuffer code = new StringBuffer();
             Object name = diagnostic.getArgs()[1];
 
             final int position[] = new int[1];
@@ -204,7 +197,7 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
                 @Override
                 public Void visitClassDeclaration(ClassDeclarationTree node, Void p) {
                     this.currentClass = node;
-                    
+
                     return super.visitClassDeclaration(node, p);
                 }
 
@@ -212,9 +205,8 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
                 public Void visitMethodInvocation(FunctionInvocationTree node, Void p) {
                     SourcePositions sourcePositions = compilationInfo.getTrees().getSourcePositions();
                     int startPosition = (int) sourcePositions.getStartPosition(compilationInfo.getCompilationUnit(), node);
-                    ((JCDiagnostic)diagnostic.getArgs()[4]).getEndPosition();
                     if (startPosition == diagnostic.getStartPosition()) {
-                        position[0] = (int) sourcePositions.getEndPosition(compilationInfo.getCompilationUnit(), currentClass.getClassMembers().get(currentClass.getClassMembers().size() -1 ));
+                        position[0] = (int) sourcePositions.getEndPosition(compilationInfo.getCompilationUnit(), currentClass.getClassMembers().get(currentClass.getClassMembers().size() - 1));
 
                         return null;
                     }
@@ -227,51 +219,129 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
             if (space.length() > 0) {
                 space = space.substring(0, space.length() - 1);
             }
-            function.append("\n\n"); //NOI18N
-            function.append(space).append("function ").append(name).append("() {\n");
-            function.append(space).append(HintsUtils.TAB).append("throw new UnsupportedOperationException('Not implemented yet');\n"); //NOI18N
-            function.append(space).append("}\n"); //NOI18N
+            code.append("\n\n"); //NOI18N
+            code.append(space).append("function ").append(name).append("() {\n"); //NOI18N
+            code.append(space).append(HintsUtils.TAB).append("throw new UnsupportedOperationException('Not implemented yet');\n"); //NOI18N
+            code.append(space).append("}\n"); //NOI18N
 
-            return new GeneratedCode(position[0], function.toString());
+            return new GeneratedCode(position[0], code.toString());
         }
 
-        private GeneratedCode createVariable(final JCDiagnostic diagnostic) {
-            StringBuffer function = new StringBuffer();
-            Object name = diagnostic.getArgs()[1];
+        private GeneratedCode createVariable(final JCDiagnostic diagnostic, Kind kind) {
+            Object varName = diagnostic.getArgs()[1];
+            GeneratedCode generatedCode = null;
+            if (kind == Kind.LOCAL_VARIABLE) {
+                generatedCode = generateLocalVar(varName.toString());
+            } else if (kind == Kind.VARIABLE) {
+                generatedCode = generateGlobalVar(varName.toString());
+            }
+            
+            return generatedCode;
+        }
 
+        private GeneratedCode generateLocalVar(String varName) {
             final int position[] = new int[1];
+            final SourcePositions sourcePositions = compilationInfo.getTrees().getSourcePositions();
             new JavaFXTreePathScanner<Void, Void>() {
 
                 @Override
                 public Void visitIdentifier(IdentifierTree node, Void p) {
-                    SourcePositions sourcePositions = compilationInfo.getTrees().getSourcePositions();
                     int startPosition = (int) sourcePositions.getStartPosition(compilationInfo.getCompilationUnit(), node);
                     if (startPosition == diagnostic.getStartPosition()) {
-                        Tree parentTree = getCurrentPath().getParentPath().getLeaf();
-                        position[0] = (int) sourcePositions.getStartPosition(compilationInfo.getCompilationUnit(), parentTree) + 1;
-                        if (position[0] < startPosition) {
-                            position[0] = startPosition;
-                        }
+                        position[0] = startPosition;
+
                         return null;
                     }
+
                     return super.visitIdentifier(node, p);
                 }
             }.scan(compilationInfo.getCompilationUnit(), null);
-
             String space = HintsUtils.calculateSpace(position[0], document);
-            function.append("\n");
-            function.append(space).append(HintsUtils.TAB).append("var ").append(name).append(";");
+            StringBuffer code = new StringBuffer().append("var ").append(varName).append(";\n").append(space); //NOI18N
 
-
-            return new GeneratedCode(position[0], function.toString());
+            return new GeneratedCode(position[0], code.toString());
         }
 
-        private String createInnerClass() {
-            StringBuffer function = new StringBuffer();
-            Object name = diagnostic.getArgs()[1];
-            function.append("class ").append(name).append("{};\n");
+        private GeneratedCode generateGlobalVar(final String varName) {
+            final int position[] = new int[1];
+            final SourcePositions sourcePositions = compilationInfo.getTrees().getSourcePositions();
+            final StringBuffer code = new StringBuffer();
+            new JavaFXTreePathScanner<Void, Void>() {
 
-            return function.toString();
+                private ClassDeclarationTree currentClass;
+
+                @Override
+                public Void visitClassDeclaration(ClassDeclarationTree node, Void p) {
+                    this.currentClass = node;
+
+                    return super.visitClassDeclaration(node, p);
+                }
+
+                @Override
+                public Void visitIdentifier(IdentifierTree node, Void p) {
+                    int startPosition = (int) sourcePositions.getStartPosition(compilationInfo.getCompilationUnit(), node);
+                    if (startPosition == diagnostic.getStartPosition()) {
+                        int start = (int) sourcePositions.getStartPosition(compilationInfo.getCompilationUnit(), currentClass);
+                        Tree firstVar = null;
+                        Iterator<Tree> iterator = currentClass.getClassMembers().iterator();
+                        if (iterator.hasNext()) {
+                            Tree tree = iterator.next();
+                            if (tree instanceof JFXVar) {
+                                firstVar = tree;
+                            }
+                        }
+                        if (firstVar != null) {
+                            position[0] = (int) sourcePositions.getStartPosition(compilationInfo.getCompilationUnit(), firstVar);
+                            String space = HintsUtils.calculateSpace(position[0], document);
+                            code.append("var ").append(varName).append(";\n").append(space); //NOI18N
+
+                            return null;
+                        }
+                        try {
+                            //TODO Line below returns 0 in same cases which means start of the node and end of the node is the same which is not true.
+                            //int length = (int) sourcePositions.getEndPosition(compilationInfo.getCompilationUnit(), currentClass) - start;
+                            //TODO Workaround for this problem
+                            String sourceCode = document.getText(start, document.getLength() - start);
+                            int index = sourceCode.indexOf("{"); //NOI18N
+                            position[0] = start + index + 1;
+                            String space = HintsUtils.calculateSpace(position[0], document);
+                            code.append("\n").append(space).append(HintsUtils.TAB).append("var ").append(varName).append(";"); //NOI18N
+                        } catch (BadLocationException ex) {
+                            log.severe(ex.getMessage());
+                        }
+
+                        return null;
+                    }
+
+                    return super.visitIdentifier(node, p);
+                }
+            }.scan(compilationInfo.getCompilationUnit(), null);
+            
+            return diagnostic.getStartPosition() < position[0] ? generateLocalVar(varName) : new GeneratedCode(position[0], code.toString());
+        }
+
+        private GeneratedCode createLocalClass() {
+            StringBuffer code = new StringBuffer();
+            Object name = diagnostic.getArgs()[1];
+           
+
+            final int position[] = new int[1];
+            final SourcePositions sourcePositions = compilationInfo.getTrees().getSourcePositions();
+            new JavaFXTreePathScanner<Void, Void>() {
+
+                @Override
+                public Void visitClassDeclaration(ClassDeclarationTree node, Void p) {
+                    position[0] = (int) sourcePositions.getEndPosition(compilationInfo.getCompilationUnit(), node);
+                    return null;
+                }
+
+            }.scan(compilationInfo.getCompilationUnit(), null);
+            code.append("\n"); //NOI18N
+            code.append("\nclass ").append(name).append(" {\n"); //NOI18N
+            code.append(HintsUtils.TAB).append("//TODO Not implemented yet.\n"); //NOI18N
+            code.append("}\n"); //NOI18N
+
+            return new GeneratedCode(position[0], code.toString());
         }
     }
 
@@ -294,3 +364,4 @@ public final class CreateNewElementTaskFactory extends EditorAwareJavaFXSourceTa
         }
     }
 }
+
