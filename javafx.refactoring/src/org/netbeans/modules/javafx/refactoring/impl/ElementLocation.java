52f9a28fe7dd16d5b9e4ade70ff9d3f6c1b20388
Jaroslav Bachorik <yardus@netbeans.org>
2010-02-09 14:30:12 +0100

    source code cleanup

--- javafx.refactoring/src/org/netbeans/modules/javafx/refactoring/impl/ElementLocation.java
+++ javafx.refactoring/src/org/netbeans/modules/javafx/refactoring/impl/ElementLocation.java
@@ -1,6 +1,29 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *  Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ *  The contents of this file are subject to the terms of either the GNU
+ *  General Public License Version 2 only ("GPL") or the Common
+ *  Development and Distribution License("CDDL") (collectively, the
+ *  "License"). You may not use this file except in compliance with the
+ *  License. You can obtain a copy of the License at
+ *  http://www.netbeans.org/cddl-gplv2.html
+ *  or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ *  specific language governing permissions and limitations under the
+ *  License.  When distributing the software, include this License Header
+ *  Notice in each file and include the License file at
+ *  nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ *  particular file as subject to the "Classpath" exception as provided
+ *  by Sun in the GPL Version 2 section of the License file that
+ *  accompanied this code. If applicable, add the following below the
+ *  License Header, with the fields enclosed by brackets [] replaced by
+ *  your own identifying information:
+ *  "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ *  Contributor(s):
+ *
+ *  Portions Copyrighted 1997-2009 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.javafx.refactoring.impl;
@@ -9,7 +32,6 @@ import com.sun.javafx.api.tree.JavaFXTreePath;
 import com.sun.javafx.api.tree.Tree.JavaFXKind;
 import com.sun.tools.javafx.tree.JFXOverrideClassVar;
 import java.lang.ref.WeakReference;
-import java.util.EnumSet;
 import javax.lang.model.element.Element;
 import org.netbeans.api.javafx.lexer.JFXTokenId;
 import org.netbeans.api.javafx.source.CompilationInfo;
@@ -129,7 +151,7 @@ final public class ElementLocation {
 
     @Override
     public String toString() {
-        return element.toString() + " @ " + startPosition + " in " + sourceFile.getPath();
+        return element.toString() + " @ " + startPosition + " in " + sourceFile.getPath(); // NOI18N
     }
 
     private void setPositions(int pos, CompilationInfo ci) {
