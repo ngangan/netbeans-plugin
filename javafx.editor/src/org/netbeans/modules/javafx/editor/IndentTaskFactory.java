351e7b1a4f5b20d56a98abd36336e3178921d358
manowar <manowar@netbeans.org>
2010-03-17 17:55:30 +0100

    issue #169709 has been fixed

--- javafx.editor/src/org/netbeans/modules/javafx/editor/IndentTaskFactory.java
+++ javafx.editor/src/org/netbeans/modules/javafx/editor/IndentTaskFactory.java
@@ -35,21 +35,20 @@ import java.util.logging.Level;
 
 /**
  * @author Rastislav Komara (<a href="mailto:rastislav.komara@sun.com">RKo</a>)
- * @todo documentation
  */
 public class IndentTaskFactory implements IndentTask.Factory {
 
     private static Logger log = Logger.getLogger(IndentTaskFactory.class.getName());
 
     /**
-     * Create reformatting task.
+     * Create indentation task.
      *
      * @param context non-null indentation context.
-     * @return reformatting task or null if the factory cannot handle the given context.
+     * @return indentation task or null if the factory cannot handle the given context.
      */
     public IndentTask createTask(Context context) {
         if (log.isLoggable(Level.FINE)) {
-            log.fine("Creating reformat factory"); // NOI18N
+            log.fine("Creating indentation factory"); // NOI18N
         }
         return new CompilerIndependentJFXIndentTask(context);
     }
